4441c53c6a331818acaf66cf3e53e3c8cfc4a728
piter2519 <piochaj@outlook.com>
2025-06-02 23:46:14 +0200

    Dziala

---
+++ Model.h
@@ -0,0 +1,26 @@
+// === Model.h ===
+#pragma once
+
+#include <vector>
+#include <string>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+#include <optional>
+
+class Model {
+public:
+    VAO vao;
+    std::optional<VBO> vbo;
+    std::optional<EBO> ebo;
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
+
+    Model(const std::string& path);
+    void Draw(Shader& shader);
+};
+
+
